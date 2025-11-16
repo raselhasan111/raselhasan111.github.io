be904a43abc84a19dd5b760d19bfc6ef7fbe30c0
Rasel Hasan <rasel.hasan@vivasoftltd.com>
2025-11-16 19:47:27 +0600

    Update site URL in configuration to reflect new deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://raselhasan111.github.io',
+  site: 'https://raselhasan.vercel.app',
   base: '/',
   vite: {
     plugins: [tailwindcss()],
